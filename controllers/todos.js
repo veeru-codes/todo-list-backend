26afa98f9fadc9a2a4fb00b709813b22fb4e4e07
Veerendra <pvnreddy1997@gmail.com>
2023-04-06 17:50:06 +0530

    Add remaining methods

--- controllers/todos.js
+++ controllers/todos.js
@@ -3,9 +3,9 @@ const pool = require('../db/connect.js')
 const getAllTodos = async (req, res) => {
   try {
     const todos = await pool.query('SELECT * FROM todo ')
-    res.status(200).json({ data: todos })
+    res.status(200).json({ data: todos.rows })
   } catch (error) {
-    console.log('Error: ', error.message)
+    res.statu(400).json({ error: error.message })
   }
 }
 
@@ -19,15 +19,48 @@ const createTodo = async (req, res) => {
 
     res.status(201).json({ msg: 'Todo created' })
   } catch (error) {
-    console.log('Error: ', error.message)
+    res.status(400).json({ error: error.message })
   }
 }
 
-const getTodo = async (req, res) => {}
+const getTodo = async (req, res) => {
+  try {
+    const { id } = req.params
 
-const updateTodo = async (req, res) => {}
+    console.log(typeof id)
 
-const deleteTodo = async (req, res) => {}
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id])
+    res.status(200).json({ data: todo.rows[0] })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body
+
+    await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2 ', [
+      description,
+      id
+    ])
+
+    res.status(200).json({ data: 'Todo updated' })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params
+    await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
+    res.status(200).json({ data: 'Todo deleted' })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
 
 module.exports = {
   getAllTodos,
